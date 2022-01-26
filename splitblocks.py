60452bd4ac152acc93f3438ce34214319f4c1c21
Kabilan S <45006244+kabilan1290@users.noreply.github.com>
2022-01-26 22:41:45 +0530

    Create splitblocks.py

---
+++ splitblocks.py
@@ -0,0 +1,9 @@
+def splitblocks(c):
+	order = len(c)/16
+	chunks, chunk_size = len(c), len(c)/order
+	a = [ c[i:i+chunk_size] for i in range(0, chunks, chunk_size) ]
+	raw_blocks = a
+	blocks = []
+	for i in range(0,order):
+		blocks.append(a[i].encode("hex"))
+	return blocks
